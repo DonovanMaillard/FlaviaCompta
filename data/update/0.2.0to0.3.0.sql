400478fb32994cbef292a73d2142b00d3db660f3
DonovanMaillard <donovan.maillard@gmail.com>
2022-09-15 16:22:30 +0200

    update migration data + fix typo

--- data/update/0.2.0to0.3.0.sql
+++ data/update/0.2.0to0.3.0.sql
@@ -32,3 +32,21 @@ AS SELECT b.id_budget,
      LEFT JOIN comptasso.cor_action_budget cab ON cab.id_budget = b.id_budget
   GROUP BY b.id_budget, b.name, b.reference, f.id_funder, f.name, bt.label, b.date_max_expenditure, b.date_return, b.budget_amount, b.payroll_limit, b.indirect_charges, b.comment, b.active, (comptasso.get_sum_movement(b.id_budget, 'Recette'::text)), ((comptasso.get_sum_movement(b.id_budget, 'Recette'::text) / b.budget_amount * 100::numeric)::numeric(8,2)), (comptasso.get_sum_movement(b.id_budget, 'Dépense'::text)), ((comptasso.get_sum_movement(b.id_budget, 'Dépense'::text) / b.budget_amount * 100::numeric)::numeric(8,2)), (comptasso.get_sum_movement(b.id_budget, 'Engagement'::text)), ((comptasso.get_sum_movement(b.id_budget, 'Engagement'::text) / b.budget_amount * 100::numeric)::numeric(8,2)), (b.budget_amount - (comptasso.get_sum_movement(b.id_budget, 'Dépense'::text) + comptasso.get_sum_movement(b.id_budget, 'Engagement'::text)))
   ORDER BY b.active DESC, b.name ASC;
+
+-- Done on server
+INSERT INTO comptasso.dict_categories (cd_category, "label", detail, "level", cd_broader, id_type_operation, seizable)
+VALUES (65, 'Autres charges de gestion courante (redevances, cotisations...)', 'Autres charges liées aux activités et à la gestion courante de l''association : redevances, cotisations, subventions versées ...',1,65,(SELECT id_type_operation FROM comptasso.dict_operation_types WHERE "label"='Dépense'),true ),
+(67, 'Charges exceptionnelles (amendes, créances, pénalités...)', 'Charges exceptionnelles : créances, amendes, pénalités, rappels d''imposition...',1,65,(SELECT id_type_operation FROM comptasso.dict_operation_types WHERE "label"='Dépense'),true );
+
+INSERT INTO comptasso.dict_categories (cd_category, label, detail, level, cd_broader, id_type_operation, seizable)
+VALUES
+('648','Autres charges de personnels (gratifications, indemnités de stage...)','Gratifications et indemnités de stages, services civiques etc',2,64,(SELECT id_type_operation FROM comptasso.dict_operation_types WHERE label='Dépense'),true),
+
+UPDATE comptasso.dict_categories
+SET seizable=true,
+label = 'Charges de personnel (salaires, primes, prélèvements urssaf...)'
+WHERE cd_category=64;
+
+UPDATE comptasso.dict_categories
+SET seizable=false
+WHERE cd_category IN (641,645);
