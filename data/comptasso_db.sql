8cb4884304c2cd87f1fe5e7cc94c1576415e9fd7
DonovanMaillard <donovan.maillard@gmail.com>
2023-09-27 18:53:24 +0200

    add view for results by year

--- data/comptasso_db.sql
+++ data/comptasso_db.sql
@@ -283,8 +283,8 @@ $function$
 ;
 
 
-  CREATE OR REPLACE FUNCTION comptasso.get_payrolls(cur_id_member integer, cur_id_budget integer)
- RETURNS TABLE (
+CREATE OR REPLACE FUNCTION comptasso.get_payrolls(cur_id_member integer, cur_id_budget integer)
+RETURNS TABLE (
   id_payroll integer,
   date_min_period date,
   date_max_period date,
@@ -570,6 +570,49 @@ LEFT JOIN LATERAL ( SELECT count(*)
             WHERE NOT active
             GROUP BY id_activity) b2 ON b1.id_activity=a.id_activity
 GROUP BY a.id_activity, a.label, a.description, a.active
+);
+
+
+CREATE VIEW comptasso.v_result_by_year AS (
+WITH years AS (
+	SELECT generate_series(min(EXTRACT('year' FROM op.effective_date))::integer,max(EXTRACT('year' FROM op.effective_date))::integer) AS year
+	FROM comptasso.t_operations op
+),
+details AS (
+	SELECT DISTINCT 
+	y.YEAR,
+	typ.LABEL AS type_category,
+	cat.LEVEL AS level,
+	cat.cd_category::TEXT AS cd_category,
+	cat.cd_category||'. '||cat.LABEL AS label,
+	cat.cd_broader,
+	sum(op.amount) AS sum,
+	jsonb_build_object('category',cat.cd_category||'. '||cat.LABEL,'result',sum(op.amount)) AS json
+FROM comptasso.dict_categories cat
+JOIN comptasso.dict_operation_types typ ON cat.id_type_operation = typ.id_type_operation
+CROSS JOIN years y
+LEFT JOIN LATERAL (
+	SELECT * 
+	FROM comptasso.t_operations op
+	JOIN comptasso.dict_operation_types typ ON op.id_type_operation = typ.id_type_operation 
+	WHERE EXTRACT('year' FROM op.effective_date)::integer=y.YEAR::integer
+	AND op.id_category = cat.id_category 
+	) op ON TRUE
+WHERE typ.LABEL IN ('Recette','Dépense')
+GROUP BY 1,2,3,4,5,6
+ORDER BY 1,2,4,5
+)
+SELECT DISTINCT 
+	d.YEAR,
+	d.type_category,
+	d.cd_category,
+	d.cd_category||'. '||d.LABEL,
+	jsonb_agg(child.json),
+	sum(child.sum)
+FROM details d
+JOIN details child ON d.cd_category::text=child.cd_broader::text
+WHERE d.LEVEL=1 AND child.YEAR=d.year
+GROUP BY 1,2,3,4
 );
 
 -----------------------------------
